1a79be2f10e9faa6e9fbc37c1b0739c50b2e6a78
demmonico <10497315+demmonico@users.noreply.github.com>
2025-04-05 12:44:35 +0200

    ADD workflow

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 setup(
     name='mysql_checksum',
     author='demmonico',
-    version='0.7',
+    version='0.8',
     long_description=description,
     long_description_content_type='text/markdown',
 
