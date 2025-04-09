3a400a6a4d7ddff6ade98bb1c742c17c7382eb7b
demmonico <10497315+demmonico@users.noreply.github.com>
2025-04-09 09:30:56 +0200

    Bump version: 0.9.7 → 0.9.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 setup(
     name='mysql_checksum',
     author='demmonico',
-    version='0.9.7',
+    version='0.9.8',
     long_description=description,
     long_description_content_type='text/markdown',
 
